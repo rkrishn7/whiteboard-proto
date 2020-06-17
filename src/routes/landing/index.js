00c58bf521182f1a4890cbe9ba54c13e5dc92446
jyoshio2 <galdones@usc.edu>
2020-06-17 11:44:38 -0700

    updated landing page

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import './index.css';
 
-import {
-    Link
-} from "react-router-dom";
+export default function Landing() {
+  const [showJoinModal, setShowJoinModal] = useState(false);
+  const [showCreateModal, setShowCreateModal] = useState(false);
 
-function Landing() {
+  function handleShow(button) {
+    if (button === 'join') {
+      setShowJoinModal(true);
+    } else if (button === 'create') {
+      setShowCreateModal(true);
+    }
+  };
 
-  return (
-    <div>
-        <Link to="/whiteboard">
-            Sesh
-        </Link>
-    </div>
-  );
+  function handleClose(button) {
+    if (button === 'join') {
+      setShowJoinModal(false);
+    } else if (button === 'create') {
+      setShowCreateModal(false);
+    }
+  }
+
+  return( 
+    <>
+      <div id='main-container'>
+        <div id='main-text'>
+          <h1>WhiteBoard</h1>
+        </div>
+        <div id='main-buttons'>
+          <Button variant='outline-primary' onClick={ () => handleShow('join') }>Join a Session</Button>{' '}
+          <Button variant='outline-success' onClick={ () => handleShow('create') }>Create a Session</Button>{' '}
+        </div>
+      </div>
+      <Modal
+        show={showJoinModal}
+        onHide={ () => handleClose('join') }
+        keyboard={ false }
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Join a WhiteBoard!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          
+        </Modal.Body>
+      </Modal>
+      <Modal
+        show={showCreateModal}
+        onHide={ () => handleClose('create') }
+        keyboard={ false }
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Create a WhiteBoard!</Modal.Title>
+        </Modal.Header>
+      </Modal>
+    </>
+  )
 }
-
-export default Landing;
